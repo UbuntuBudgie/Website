ca5885fa9a856452ee276bc6b6ee3cb39c980937
GrindamN <nikolas@grindam.com>
2026-01-25 00:07:25 +0100

    Mini change

--- .astro/content.d.ts
+++ .astro/content.d.ts
@@ -202,6 +202,16 @@ declare module 'astro:content' {
   rendered?: RenderedContent;
   filePath?: string;
 }>;
+"pages": Record<string, {
+  id: string;
+  render(): Render[".md"];
+  slug: string;
+  body: string;
+  collection: "pages";
+  data: InferEntrySchema<"pages">;
+  rendered?: RenderedContent;
+  filePath?: string;
+}>;
 "team": Record<string, {
   id: string;
   render(): Render[".md"];
